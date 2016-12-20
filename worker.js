70c272faa9a1d98a89c97dafbec402a7d160a55e
Prashanth Chandra <coolshanth94@gmail.com>
2016-12-21 00:31:57 +0800

    Add ws handlers for create/save/getNote

--- worker.js
+++ worker.js
@@ -6,6 +6,7 @@ const serveStatic = require('serve-static');
 const path = require('path');
 const morgan = require('morgan');
 const healthChecker = require('sc-framework-health-check');
+const uuid = require('./lib/uuid');
 const bcrypt = require('bcrypt');
 const MongoClient  = require('mongodb').MongoClient;
 const dbUrl = 'mongodb://localhost:27017/cheatsheet';
@@ -34,33 +35,101 @@ module.exports.run = async function (worker) {
 
   httpServer.on('request', app);
 
+  scServer.addMiddleware(scServer.MIDDLEWARE_SUBSCRIBE, function (req, next) {
+    const publishOnly = ['saveNote'];
+    if (publishOnly.includes(req.channel)) {
+      next('Cannot subscribe to channel ' + req.channel);
+    }
+  });
+
+  scServer.addMiddleware(scServer.MIDDLEWARE_PUBLISH_IN, function (req, next) {
+    next();
+  });
+
   /*
     In here we handle our incoming realtime connections and listen for events.
   */
   scServer.on('connection', function (socket) {
     socket.on('login', async function (credentials, respond) {
       let success, error;
+      const username = credentials.username;
+      const password = credentials.password;
       try {
-        const {username, password} = credentials;
-        const {hash} = await users.findOne({username});
-        success = await bcrypt.compare(password, hash);
+        const user = await users.findOne({username});
+        success = await bcrypt.compare(password, user.hash);
       } catch (e) {
         error = e;
       } finally {
-        if (error || !success) {
-          respond('Login failed');
+        const isValidLogin = success && !error;
+        if (isValidLogin) {
+          socket.setAuthToken({username, channels: ['saveNote', 'createNote']});
+
+          respond();
         } else {
-          respond('Login success');
+          respond('Login failed');
         }
       }
     });
 
-    socket.on('saveNote', async function (note, respond) {
-      // TODO save note;
+    socket.on('createNote', async function (request, respond) {
+      const token = socket.getAuthToken();
+      let note, error;
+      if (token) {
+        note = {
+          name: 'Untitled note',
+          contents: '',
+          updatedAt: Date.now()
+        };
+        const id = uuid()
+        try {
+          await users.updateOne({username: token.username}, {$set: {['notes.' + id]: note}});
+        } catch (e) {
+          error = e;
+        } finally {
+          if (error) {
+            respond(error.message);
+          } else {
+            note.id = id;
+            respond(null, note);
+          }
+        }
+      } else {
+        respond('Not authenticated');
+      }
     });
 
+    socket.on('saveNote', async function (note, respond) {
+      const token = socket.getAuthToken();
+      const id = note.id;
+      const contents = note.contents;
+      delete note.id;
+      let error;
+      if (token) {
+        try {
+          await users.updateOne({username: token.username}, {$set: {[`notes.${id}.contents`]: contents}});
+        } catch (e) {
+          error = e;
+        } finally {
+          if (error) {
+            respond(error.message);
+          } else {
+            respond(null, null);
+          }
+        }
+      } else {
+        respond('Not authenticated');
+      }
+    });
+
+    socket.on('getNote', async function (id, respond) {
+      // TODO
+      // const token = user.getAuthToken();
+      //
+      // const user = await users.findOne({username: token.username})
+    })
+
     socket.on('disconnect', function () {
-      clearInterval(interval);
+      // cleanup code in the future
     });
   });
 };
